4135924d40dd2ee539ad903f6646494e4b9efa52
Christoph <c@kluenter.de>
2022-06-26 16:00:07 +0200

    adds monitoring endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_prom::{PrometheusMetricsBuilder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -10,16 +11,25 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[get("/health")]
+async fn health() -> impl Responder { HttpResponse::Ok().finish()}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let prometheus = PrometheusMetricsBuilder::new("api")
+        .endpoint("/metrics")
+        .build()
+        .unwrap();
+    HttpServer::new(move || {
         App::new()
+            .wrap(prometheus.clone())
             .service(hello)
             .service(echo)
+            .service(health)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", 8080))?
