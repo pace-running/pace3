03f2e816b58dcdaa4869cf6209020a82539ff60c
Christoph <c@kluenter.de>
2022-08-27 16:38:12 +0200

    adds test for index page
    this ensures template rendering works

--- src/handlers/hello.rs
+++ src/handlers/hello.rs
@@ -26,3 +26,37 @@ pub async fn file(file: web::Path<String>, _req: HttpRequest) -> Result<NamedFil
     Ok(NamedFile::open(path)?)
 }
 
+#[cfg(test)]
+mod tests {
+    use tera::{Tera};
+    use actix_web::{
+        http::{StatusCode}};
+    use actix_web::body::to_bytes;
+    use actix_web::web::Bytes;
+    use crate::handlers::hello::index;
+
+    trait BodyTest {
+        fn as_str(&self) -> &str;
+    }
+
+    impl BodyTest for Bytes {
+        fn as_str(&self) -> &str {
+            std::str::from_utf8(self).unwrap()
+        }
+    }
+
+
+    #[actix_web::test]
+    async fn start_page() {
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(_e) => {std::process::exit(1)}
+        };
+        let data = actix_web::web::Data::new(tera);
+        let resp = index(data).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+
+        let body = to_bytes(resp.into_body()).await.unwrap();
+        assert!(body.as_str().contains("<h1>Home</h1>") )
+    }
+}
