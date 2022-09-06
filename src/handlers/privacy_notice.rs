2c41a73a280e7e77307df1646d9b826a6c33a8a3
niklas-mertens <niklas.mertens@thoughtworks.com>
2022-09-06 14:42:58 +0200

    Added unit tests for imprint and privacy notice

--- src/handlers/privacy_notice.rs
+++ src/handlers/privacy_notice.rs
@@ -8,3 +8,39 @@ pub async fn show(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
     let rendered = tmpl.render("privacy_notice.html", &ctx).unwrap();
     Ok(HttpResponse::Ok().content_type("text/html").body(rendered))
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::handlers::privacy_notice::show;
+    use crate::models::event::current_event;
+    use actix_web::body::to_bytes;
+    use actix_web::http::StatusCode;
+    use actix_web::web::Bytes;
+    use tera::Tera;
+
+    trait BodyTest {
+        fn as_str(&self) -> &str;
+    }
+
+    impl BodyTest for Bytes {
+        fn as_str(&self) -> &str {
+            std::str::from_utf8(self).unwrap()
+        }
+    }
+
+    #[actix_web::test]
+    async fn privacy_page() {
+        let tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(_e) => std::process::exit(1),
+        };
+        let data = actix_web::web::Data::new(tera);
+        let resp = show(data).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+
+        let body = to_bytes(resp.into_body()).await.unwrap();
+        let event = current_event();
+        assert!(body.as_str().contains("Datenschutz"))
+    }
+}
