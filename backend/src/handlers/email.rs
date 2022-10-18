2b7ce3ae712cbd27d13e4a31e9563a7cc8edf770
niklas-mertens <niklas.mertens@thoughtworks.com>
2022-10-18 15:23:11 +0200

    Added template for payment confirmation email

--- backend/src/handlers/email.rs
+++ backend/src/handlers/email.rs
@@ -15,7 +15,9 @@ struct EmailInfo {
 
 pub fn send_registration_email(
     email: String,
+
     donation: String,
+
     reason_for_payment: String,
 ) -> bool {
     dotenv().ok();
@@ -66,8 +68,60 @@ pub fn send_registration_email(
     match mailer.send(&email) {
         Ok(_) => {
             println!("Email sent successfully!");
-            true
+            return true;
         }
-        Err(_) => false,
+        Err(_) => return false,
+    }
+}
+
+pub fn send_payment_confirmation(email: String, donation: String, status_link: String) -> bool {
+    dotenv().ok();
+    let sender_email =
+        std::env::var("SENDER_EMAIL").unwrap_or_else(|_| "SENDER_EMAIL must be set.".to_string());
+    let smtp_password =
+        std::env::var("SMTP_PASSWORD").unwrap_or_else(|_| "SMTP_PASSWORD must be set.".to_string());
+    let mut ctx = Context::new();
+    let email_info = EmailInfo {
+        donation,
+        reason_for_payment: String::from(""),
+        status_link,
+    };
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(_e) => std::process::exit(1),
+    };
+    let tmpl = web::Data::new(tera);
+    ctx.insert("email_info", &email_info);
+    let rendered = tmpl.render("payment_confirmation_mail.html", &ctx).unwrap();
+    if sender_email.contains("must be set") || smtp_password.contains("must be set") {
+        return false;
+    }
+    let email = Message::builder()
+        .from(
+            format!("Lauf gegen Rechts<{}>", sender_email)
+                .parse()
+                .unwrap(),
+        )
+        .to(email.parse().unwrap())
+        .subject("Lauf gegen Rechts - Zahlung bestätigt")
+        .header(header::ContentType::TEXT_HTML)
+        .body(rendered)
+        .unwrap();
+
+    let creds = Credentials::new(sender_email.to_string(), smtp_password.to_string());
+
+    // Open a remote connection to gmail
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .build();
+
+    // Send the email
+    match mailer.send(&email) {
+        Ok(_) => {
+            println!("Email sent successfully!");
+            return true;
+        }
+        Err(_) => return false,
     }
 }
