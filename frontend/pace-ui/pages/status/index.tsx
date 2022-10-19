b69fa735f47de6a38cce80705fdc753811c7a7cb
maximefloriani <maximetchangou2011@yahoo.fr>
2022-10-19 02:17:48 +0200

    Create status page placeholder

---
+++ frontend/pace-ui/pages/status/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import BaseLayout from '../../components/Layout/baseLayout';
+
+const StatusPage: NextPage = () => {
+  return (
+    <BaseLayout pageTitle='Status'>
+      <div className='container'>
+        <h1>Status Page will be implemented soon</h1>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export default StatusPage;
