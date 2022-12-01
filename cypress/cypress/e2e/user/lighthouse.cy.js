85f66b33ffe221882efd081fffe40a1a60d1703f
nvinogradova <nvinogradova8@gmail.com>
2022-12-01 10:58:02 +0100

    Lighthouse tests for imprint and admin login page added

--- cypress/cypress/e2e/user/lighthouse.cy.js
+++ cypress/cypress/e2e/user/lighthouse.cy.js
@@ -23,4 +23,24 @@ context("Main page", () => {
       performance: 50,
     });
   });
+  it("should verify the score of the imprint page", () => {
+    cy.visit("https://pace3.lauf-gegen-rechts.de/imprint");
+    cy.lighthouse({
+      accessibility: 100,
+      "best-practices": 100,
+      seo: 50,
+      pwa: 30,
+      performance: 50,
+    });
+  });
+  it("should verify the score of the admin login page", () => {
+    cy.visit("https://pace3.lauf-gegen-rechts.de/admin/login");
+    cy.lighthouse({
+      accessibility: 100,
+      "best-practices": 100,
+      seo: 50,
+      pwa: 30,
+      performance: 50,
+    });
+  });
 });
