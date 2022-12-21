f6630e7821f198b444b632037e2d1b4622687ca3
nvinogradova <nvinogradova8@gmail.com>
2022-12-21 14:09:54 +0100

    Set up Jest for frontend testing
    Co-authored-by: niklasmertens <niklasmertens97@gmail.com>

---
+++ frontend/pace-ui/pages/join/join.test.ts
@@ -0,0 +1,7 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('testing of the registration page', () => {
+  test('placeholder', () => {
+    expect(true).toBe(true);
+  });
+});
