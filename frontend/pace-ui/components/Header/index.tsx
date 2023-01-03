074eea4e4c31fb8df4172f427085510f85a05ca3
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-01-03 16:30:51 +0100

    Change the way the logo.svg is accessed, o/w breaks tests
    Co-authored-by: nvinogradova <nvinogradova8@gmail.com>

--- frontend/pace-ui/components/Header/index.tsx
+++ frontend/pace-ui/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import router from 'next/router';
-import logo from '../../public/logo.svg';
 import React from 'react';
 
 const Header: React.FC = () => {
   return (
     <nav className='navbar p-3 full brownbg py-5' style={{ marginRight: '0' }}>
       <div className='text-left fixed-top' style={{ zIndex: '-1' }}>
-        <Image src={logo} alt='FC St. Pauli Logo' height={100} width={100} />
+        <Image src='/logo.svg' alt='FC St. Pauli Logo' height={100} width={100} />
       </div>
       <div className='text-left' style={{ position: 'absolute', left: '110px' }}>
         <Link href='/'>
