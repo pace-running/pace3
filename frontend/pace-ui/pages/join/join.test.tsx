d6c1981b58bb50be82eff7037b0144f2f73d348d
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-01-05 13:26:48 +0100

    Extend join test
    Co-authored-by: nvinogradova <nvinogradova8@gmail.com>

--- frontend/pace-ui/pages/join/join.test.tsx
+++ frontend/pace-ui/pages/join/join.test.tsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 
 import Join from '.';
 import React from 'react';
+import { wait } from '@testing-library/user-event/dist/types/utils';
 
 describe('testing of the registration page', () => {
   beforeEach(() => {
@@ -17,12 +18,34 @@ describe('testing of the registration page', () => {
       expect(screen.getByRole('heading', { name: 'Anmeldung' })).toHaveTextContent('Anmeldung');
       expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Fan T-Shirt');
     });
+
+    test('initially text fields should be empty', () => {
+      expect(screen.getByRole('textbox', { name: 'Vorname (erscheint auf der Startnummer)' })).toHaveTextContent('');
+    });
+
+    test('email input field should display correct error messages', async () => {
+      const emailInput = screen.getByRole('textbox', { name: 'Email' });
+      const emailConfirmInput = screen.getByRole('textbox', { name: 'Email wiederholen' });
+      const user = userEvent.setup();
+
+      await user.type(emailInput, 'email');
+      expect(screen.getByText('E-Mail muss zulässige E-Mail-Adresse sein!'));
+      await user.type(emailInput, '@example.com');
+      expect(screen.queryByText('E-Mail muss zulässige E-Mail-Adresse sein!')).not.toBeInTheDocument();
+      expect(screen.getByText('E-Mail Adressen müssen übereinstimmen!'));
+      await user.type(emailConfirmInput, 'email@example.com');
+      // empty timeout needed, otherwise test does not wait for error message to disappear
+      await new Promise((r) => setTimeout(r,0));
+      await expect(screen.queryByText('E-Mail Adressen müssen übereinstimmen!')).not.toBeInTheDocument();
+    });
+
   });
 
   describe('submit button', () => {
     test('submit button is initially disabled', () => {
       expect(screen.getByRole('button', { name: 'Weiter' })).toBeDisabled();
     });
+
     test('accepting terms and conditions enables submit button', async () => {
       const user = userEvent.setup();
       await user.click(screen.getByText('Mir ist bewusst,', { exact: false }));
