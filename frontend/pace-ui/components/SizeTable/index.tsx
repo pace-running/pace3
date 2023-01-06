00f2d9a836d4ed0bbd5294dea52fe911a59382c8
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-01-06 16:46:15 +0100

    Fixed size table warning
    Co-authored-by: nvinogradova <nvinogradova8@gmail.com>

--- frontend/pace-ui/components/SizeTable/index.tsx
+++ frontend/pace-ui/components/SizeTable/index.tsx
@@ -18,31 +18,35 @@ const SizeTable: React.FC = () => {
             <h2>Tailliert</h2>
             <div style={{ overflowX: 'auto' }}>
               <table className='size-table center' id='slimfit_table'>
-                <tr>
-                  <th>Größe</th>
-                  <th>Weite</th>
-                  <th>Länge</th>
-                </tr>
-                <tr>
-                  <td>S</td>
-                  <td>41</td>
-                  <td>61</td>
-                </tr>
-                <tr>
-                  <td>M</td>
-                  <td>44</td>
-                  <td>63</td>
-                </tr>
-                <tr>
-                  <td>L</td>
-                  <td>47</td>
-                  <td>65</td>
-                </tr>
-                <tr>
-                  <td>XL</td>
-                  <td>50</td>
-                  <td>67</td>
-                </tr>
+                <thead>
+                  <tr>
+                    <th>Größe</th>
+                    <th>Weite</th>
+                    <th>Länge</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>S</td>
+                    <td>41</td>
+                    <td>61</td>
+                  </tr>
+                  <tr>
+                    <td>M</td>
+                    <td>44</td>
+                    <td>63</td>
+                  </tr>
+                  <tr>
+                    <td>L</td>
+                    <td>47</td>
+                    <td>65</td>
+                  </tr>
+                  <tr>
+                    <td>XL</td>
+                    <td>50</td>
+                    <td>67</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </div>
@@ -52,36 +56,40 @@ const SizeTable: React.FC = () => {
             <h2>Unisex</h2>
             <div style={{ overflowX: 'auto', marginBottom: '30px' }}>
               <table className='center size-table' id='unisex_table'>
-                <tr>
-                  <th>Größe</th>
-                  <th>Weite</th>
-                  <th>Länge</th>
-                </tr>
-                <tr>
-                  <td>S</td>
-                  <td>50</td>
-                  <td>69</td>
-                </tr>
-                <tr>
-                  <td>M</td>
-                  <td>53</td>
-                  <td>72</td>
-                </tr>
-                <tr>
-                  <td>L</td>
-                  <td>56</td>
-                  <td>74</td>
-                </tr>
-                <tr>
-                  <td>XL</td>
-                  <td>58</td>
-                  <td>76</td>
-                </tr>
-                <tr>
-                  <td>XXL</td>
-                  <td>62</td>
-                  <td>78</td>
-                </tr>
+                <thead>
+                  <tr>
+                    <th>Größe</th>
+                    <th>Weite</th>
+                    <th>Länge</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>S</td>
+                    <td>50</td>
+                    <td>69</td>
+                  </tr>
+                  <tr>
+                    <td>M</td>
+                    <td>53</td>
+                    <td>72</td>
+                  </tr>
+                  <tr>
+                    <td>L</td>
+                    <td>56</td>
+                    <td>74</td>
+                  </tr>
+                  <tr>
+                    <td>XL</td>
+                    <td>58</td>
+                    <td>76</td>
+                  </tr>
+                  <tr>
+                    <td>XXL</td>
+                    <td>62</td>
+                    <td>78</td>
+                  </tr>
+                </tbody>
               </table>
             </div>
           </div>
