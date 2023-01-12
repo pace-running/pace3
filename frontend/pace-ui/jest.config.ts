4e3eb5d78a835f4f6d4509c17fee3a16cde4adca
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-01-12 15:10:46 +0100

    Jest now collects code coverage in frontend
    Co-authored-by: nvinogradova <nvinogradova8@gmail.com>

--- frontend/pace-ui/jest.config.ts
+++ frontend/pace-ui/jest.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  collectCoverage: true,
+  coverageReporters: ['json','html','text-summary'],
+  collectCoverageFrom: [
+    '**/*.tsx',
+    '!**/node_modules/**',
+  ],
   verbose: true,
   setupFilesAfterEnv: ['./jest-setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
