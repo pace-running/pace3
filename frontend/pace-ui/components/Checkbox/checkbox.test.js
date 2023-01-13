10440cc535dcd45c6b8e63efa7b33f800e954ae4
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-01-13 12:44:58 +0100

    Add test for checkbox component
    Co-authored-by: nvinogradova <nvinogradova8@gmail.com>
    Co-authored-by: sweetsacura <s.spoerkel@googlemail.com>

---
+++ frontend/pace-ui/components/Checkbox/checkbox.test.js
@@ -0,0 +1,74 @@
+import { describe, expect, test } from '@jest/globals';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import Checkbox from '.';
+
+describe('test checkbox', () => {
+  test('checkbox can be checked and unchecked', async () => {
+    let checkboxStatus = false;
+    const changeCheckBoxStatus = () => {
+      checkboxStatus = !checkboxStatus;
+    };
+    const { rerender } = render(
+      <Checkbox name='test-checkbox' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).not.toBeChecked();
+    expect(screen.getByLabelText('checkbox-label')).toEqual(checkbox);
+
+    await userEvent.click(checkbox);
+    rerender(
+      <Checkbox name='test-checkbox' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+
+    expect(checkboxStatus).toBe(true);
+    await waitFor(() => {
+      expect(checkbox).toBeChecked();
+    });
+
+    await userEvent.click(checkbox);
+    rerender(
+      <Checkbox name='test-checkbox' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+
+    await waitFor(() => {
+      expect(checkbox).not.toBeChecked();
+    });
+    expect(checkboxStatus).toBe(false);
+  });
+
+  test('switch checkbox can be checked and unchecked', async () => {
+    let checkboxStatus = false;
+    const changeCheckBoxStatus = () => {
+      checkboxStatus = !checkboxStatus;
+    };
+    const { rerender } = render(
+      <Checkbox name='test-checkbox' role='switch' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+    const toggle = screen.getByRole('switch');
+    expect(toggle).not.toBeChecked();
+    expect(screen.getByLabelText('checkbox-label')).toEqual(toggle);
+
+    await userEvent.click(toggle);
+    rerender(
+      <Checkbox name='test-checkbox' role='switch' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+
+    expect(checkboxStatus).toBe(true);
+    await waitFor(() => {
+      expect(toggle).toBeChecked();
+    });
+
+    await userEvent.click(toggle);
+    rerender(
+      <Checkbox name='test-checkbox' role='switch' label='checkbox-label' check={checkboxStatus} onChange={changeCheckBoxStatus} />
+    );
+
+    await waitFor(() => {
+      expect(toggle).not.toBeChecked();
+    });
+    expect(checkboxStatus).toBe(false);
+  });
+});
