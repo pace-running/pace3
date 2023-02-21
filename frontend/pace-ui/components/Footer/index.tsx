dffefdb8d3bd0f268eaee18401980eaa3362b3cd
Mahrukh Anwari <mahrukh.anwari@thoughtworks.com>
2023-02-21 17:15:17 +0100

    improved footer

--- frontend/pace-ui/components/Footer/index.tsx
+++ frontend/pace-ui/components/Footer/index.tsx
@@ -1,25 +1,35 @@
 import Link from 'next/link';
 import React from 'react';
 import { MDBFooter} from 'mdb-react-ui-kit';
+import Container from 'react-bootstrap/Container';
 
 const Footer: React.FC = () => {
   return (
     <MDBFooter className='text-center text-lg-start text-muted fixed-bottom brownbg' >
-    <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-      <Link className='text-reset fw-bold' href='/imprint'>
-        <a style={{ color: 'white'}}> 
-          Impressum
-        </a>
-      </Link>
-    </div>
-    
-    <div className='text-center p-4'> 
-      <Link className='text-reset fw-bold' href='/privacy_notice'>
-       <a style={{ color: 'white'}}>
-          Datenschutz
-       </a>
-      </Link>
-    </div>
+      <section className='d-flex p-4 border-bottom'>
+        <Container style ={{ width: '70%'}}>
+          <span className='text-lg-center p-3 flex-column' style={{ color: '#FFFFFF', margin: 'auto', fontSize: 19}}>
+              Lauf gegen rechts
+          </span>
+        </Container>
+        <Container style ={{ width: '70%'}}>
+          <span className='text-center p-3 flex-column'>
+            <Link href='/imprint'>
+              <a className='styled-link'>
+                Impressum
+              </a>
+            </Link>
+          </span>
+          <span className='text-center p-3 flex-column'>
+            <Link  href='/privacy_notice'>
+              <a className='styled-link'>
+                Datenschutz
+              </a>
+            </Link>
+          </span>
+        </Container>
+      </section>
+
 </MDBFooter>
   );
 };
