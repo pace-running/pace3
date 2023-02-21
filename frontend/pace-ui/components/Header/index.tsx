1e8122d58c1c5feac0302beee2d36846c51ff165
Mahrukh Anwari <mahrukh.anwari@thoughtworks.com>
2023-02-21 18:32:20 +0100

    WIP: home page

--- frontend/pace-ui/components/Header/index.tsx
+++ frontend/pace-ui/components/Header/index.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <Navbar className='navigation-bar' expand='lg'>
+    <Navbar className='navigation-bar fixed-top' expand='lg'>
         <Container >
             <Navbar.Brand href = '/'>
                 <span className='logo'>
