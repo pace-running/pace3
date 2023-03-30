9c46671683ad1360ec3f0e3409e525a13f1a221b
niklas-mertens <niklas.mertens@thoughtworks.com>
2023-03-30 17:30:27 +0200

    Edit e2e test for changing password to utilize logout button

--- cypress/cypress/e2e/user/changePassword.cy.js
+++ cypress/cypress/e2e/user/changePassword.cy.js
@@ -6,18 +6,21 @@ describe("changing the password", () => {
     cy.get('[data-testid="btn-confirm-new-password"]').click();
   };
 
-  it("can change password and login with it", () => {
+  beforeEach(() => {
     cy.clearCookies();
     cy.visit("/admin/login");
     cy.get("#username_input").type("admin");
     cy.get("#password_input").type("xoh7Ongui4oo");
     cy.get('[data-testid="btn-login"]').click();
+  });
 
+  it("can change password and login with it", () => {
     cy.get('[data-testid="btn-change-password"]').click();
     changePassword("xoh7Ongui4oo", "newPassword");
-    cy.location('pathname').should('eq', '/admin')
+    cy.location("pathname").should("eq", "/admin");
 
-    cy.clearCookies();
+    cy.get('[data-testid="logout-btn"]').click();
+
     cy.visit("/admin/login");
     cy.get("#username_input").type("admin");
     cy.get("#password_input").type("newPassword");
@@ -30,19 +33,13 @@ describe("changing the password", () => {
   });
 
   it("shows an error when specifying the wrong old password", () => {
-    cy.clearCookies();
-    cy.visit("/admin/login");
-    cy.get("#username_input").type("admin");
-    cy.get("#password_input").type("xoh7Ongui4oo");
-    cy.get('[data-testid="btn-login"]').click();
-
     cy.get('[data-testid="btn-change-password"]').click();
     // we are catching it in the original code, no idea why Cypress doesn't notice
-    cy.once('uncaught:exception', () => false);
+    cy.once("uncaught:exception", () => false);
     changePassword("wrongpassword", "newPassword");
 
     // location did not change
-    cy.location('pathname').should('eq', '/change_password')
+    cy.location("pathname").should("eq", "/change_password");
 
     cy.contains("Das alte Passwort ist nicht korrekt");
   });
