24216fbec28d8a8e8deda72e070eb6f2ce4faed5
Knut Borchers <knut.borchers@gmail.com>
2023-06-01 17:06:41 +0200

    Replace gif in loading indicator with CSS animation
    Signed-off-by: Knut Borchers <knut.borchers@gmail.com>

--- frontend/pace-ui/components/LoadingScreen/index.tsx
+++ frontend/pace-ui/components/LoadingScreen/index.tsx
@@ -4,10 +4,8 @@ import React from 'react';
 const LoadingScreen: React.FC = () => {
   return (
     <div className='loading-screen'>
-      <div>
-        <Image src='/loading-orca.gif' alt='Lade-Icon' height={500} width={500} />
-      </div>
-      <p>Seite lädt...</p>
+      <div className='loader'></div>
+      <p>Seite lädt</p>
     </div>
   );
 };
